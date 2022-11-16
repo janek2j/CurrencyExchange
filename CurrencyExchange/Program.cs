1706bf9bc9a2bfade0606a13e455bba884cd8603
Janek Jot <janek2j@gmail.com>
2022-11-16 23:09:01 +0100

    Dodaj pliki projektów.

---
+++ CurrencyExchange/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace CurrencyExchange
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
+
